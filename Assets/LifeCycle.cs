b4d67e23261168017954932ce2cd29661cd9fb5f
bada308 <gangbada890@gmail.com>
2022-09-12 22:45:54 +0900

    델타타임

--- Assets/LifeCycle.cs
+++ Assets/LifeCycle.cs
@@ -4,30 +4,18 @@ using UnityEngine;
 
 public class LifeCycle : MonoBehaviour
 {
-    Vector3 target = new Vector3(8, 1.5f, 0);
+    void Start() {
+
+    }
 
     
     void Update() {
+        Vector3 vec = new Vector3(
+            Input.GetAxisRaw("Horizontal") * Time.deltaTime,
+            Input.GetAxisRaw("Vertical") * Time.deltaTime, 0
+        );
+        transform.Translate(vec);
         
-        // 1. MoveTowards
-        /*
-        transform.position = Vector3.MoveTowards(transform.position, target, 2f);*/
-
-
-
-        // 2.SmoothDamp
-        /*
-        Vector3 velo = Vector3.zero;
-        transform.position = Vector3.SmoothDamp(transform.position, target, ref velo, 0.1f);
-        */
-
-        // 3. Lerp (선형 보간)
-        /*
-        transform.position = Vector3.Lerp(transform.position, target, 0.1f);
-        */
-
-        // 4. Slerp (구면 선형 보간)
-        transform.position = Vector3.Slerp(transform.position, target, 0.05f);
 
     }
 
@@ -35,12 +23,9 @@ public class LifeCycle : MonoBehaviour
 
 }
 
-
-
-
 /**
-MoveTowards(현재위치, 목표위치, 속도) 
-SmoothDamp(현재위치, 목표위치, 참조 속도, 속도)
-Lerp: 선형 보간, SmoothDamp보다 감속시간이 김
-Slerp: 구면 선형 보간, 호를 그리며 이동
+    Time.deltaTime 사용하는 방법
+
+    Translate: 벡터에 곱하기
+    Vector 함수: 시간 매개변수에 곱하기
 */
